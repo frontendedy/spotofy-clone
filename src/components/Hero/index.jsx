1ae4216334566fbd1d6dee8bc75f404d23786bf1
frontendedy <frontendedy@gmail.com>
2023-08-19 11:43:52 +0530

    issue resolved

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <h1 className='text-[32px] font-semibold text-white'>100 Thousand Songs, ad-free</h1>
         <h2 className='text-[32px] font-semibold text-white'>Over thousands podcast episodes</h2>
       </div>
-      <img src="/headphones.png" className='w-[212px] h-[212px]' alt="" />
+      <img src="headphones.png" className='w-[212px] h-[212px]' alt="" />
     </div>
   )
 }
